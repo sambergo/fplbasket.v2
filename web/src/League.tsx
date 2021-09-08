95f7a9388019bba51f61103ef6afeaed7eb7a391
sambergo <santeri.valikoski@gmail.com>
2021-09-08 21:15:45 +0300

    chips played

--- web/src/League.tsx
+++ web/src/League.tsx
@@ -1,22 +1,22 @@
 import { Box } from "@material-ui/core";
 import { useState } from "react";
 import Captains from "./components/Captains";
+import Chips from "./components/Chips";
 import NavBar from "./components/NavBar";
 import Players from "./components/Players";
 import Standings from "./components/Standings";
 import Transfers from "./components/Transfers";
-import { useStateValue } from "./state";
 
 type PageType = "main" | "transfers" | "standings" | "data";
 
 const League: React.FC = () => {
-  // const [{ bssData, leagueData, gwsData }] = useStateValue();
   const [page, setPage] = useState<PageType>("main");
   const pageToShow = (page: PageType) => {
     switch (page) {
       case "main":
         return (
           <>
+            <Chips />
             <Captains />
             <Players />
           </>
---
+++ web/src/components/Chips.tsx
@@ -0,0 +1,59 @@
+import {
+  CardHeader,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+import React from "react";
+import { useStateValue } from "../state";
+import CardWithTable from "./CardWithTable";
+
+const Chips: React.FC = () => {
+  const [{ bssData, leagueData }] = useStateValue();
+  if (
+    !bssData ||
+    !leagueData?.parsedData ||
+    leagueData.parsedData.chips.length < 1
+  )
+    return null;
+  const getChipName = (chip: string): string => {
+    switch (chip) {
+      case "wildcard":
+        return "Wildcard";
+      case "3xc":
+        return "Triple Captain";
+      case "freehit":
+        return "Freehit";
+      case "bboost":
+        return "Bench Boost";
+      default:
+        return "";
+    }
+  };
+  return (
+    <CardWithTable
+      header={
+        <CardHeader title={"Chips played"} style={{ textAlign: "center" }} />
+      }
+    >
+      <TableHead>
+        <TableRow>
+          <TableCell>Chip</TableCell>
+          <TableCell>Used by</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {leagueData.parsedData.chips.map((c) => {
+          return (
+            <TableRow key={c.chip}>
+              <TableCell>{getChipName(c.chip)}</TableCell>
+              <TableCell>{c.usedBy.join(", ")}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </CardWithTable>
+  );
+};
+export default Chips;
