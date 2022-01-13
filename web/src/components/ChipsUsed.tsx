e614be2b07a3822ddb3e7c4fdcdbfc313d13b097
Bergo <bergo@tutanota.com>
2022-01-13 13:57:42 +0200

    get chips used from history

--- web/src/components/ChipsUsed.tsx
+++ web/src/components/ChipsUsed.tsx
@@ -1,14 +1,13 @@
 import {
   CardHeader,
+  TableBody,
+  TableCell,
   TableHead,
   TableRow,
-  TableCell,
-  TableBody,
 } from "@material-ui/core";
 import { Box } from "@material-ui/system";
-import { FC, useEffect, useState } from "react";
-import { getTeamForChips } from "../service";
-import { Manager, GwTeam } from "../types/newleague";
+import { FC } from "react";
+import { Manager } from "../types/newleague";
 import CardWithTable from "./CardWithTable";
 import { getChipName } from "./Chips";
 
@@ -17,23 +16,6 @@ interface ChipsUsedType {
 }
 
 const ChipsUsed: FC<ChipsUsedType> = ({ manager }) => {
-  const [managerChips, setManagerChips] = useState<GwTeam[]>();
-  useEffect(() => {
-    const getteamdata = async () => {
-      const id = manager.entry.toString();
-      const chipsReq = await getTeamForChips({ id });
-      if (chipsReq.status == 200 && chipsReq.data) {
-        const data: GwTeam[] = chipsReq.data;
-        setManagerChips(data.filter((gw) => gw.active_chip));
-      }
-    };
-    getteamdata();
-  }, []);
-
-  if (!managerChips) {
-    return null;
-  }
-
   return (
     <Box>
       <CardWithTable
@@ -51,16 +33,14 @@ const ChipsUsed: FC<ChipsUsedType> = ({ manager }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {managerChips
-            ?.filter((gw) => gw.active_chip)
-            .map((gw) => {
-              return (
-                <TableRow key={gw.entry_history.event}>
-                  <TableCell>{gw.entry_history.event}</TableCell>
-                  <TableCell>{getChipName(gw.active_chip || "")}</TableCell>
-                </TableRow>
-              );
-            })}
+          {manager.history.chips.map((chip) => {
+            return (
+              <TableRow key={chip.event}>
+                <TableCell>{chip.event}</TableCell>
+                <TableCell>{getChipName(chip.name || "")}</TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </CardWithTable>
     </Box>
