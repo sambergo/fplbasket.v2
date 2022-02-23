ae42641b0d6c63cb422ba4a9b77634484bd6a2d4
Bergo <bergo@tutanota.com>
2022-02-23 22:05:50 +0200

    livebonus btn -> switch btn

--- web/src/components/ShowLiveBonusToggleButton.tsx
+++ web/src/components/ShowLiveBonusToggleButton.tsx
@@ -1,22 +1,41 @@
-import { ToggleButton } from "@material-ui/core";
+import { FormControlLabel, FormGroup, Switch } from "@material-ui/core";
 import { useStateValue } from "../state";
 
 const ShowLiveBonusToggleButton: React.FC = () => {
   const [{ showLiveBonus, showLiveBonusDisabled }, dispatch] = useStateValue();
   return (
-    <ToggleButton
-      selected={showLiveBonus && !showLiveBonusDisabled}
-      onChange={() =>
-        dispatch({
-          type: "SET_SHOW_LIVE_BONUS",
-          payload: !showLiveBonus,
-        })
-      }
-      disabled={showLiveBonusDisabled}
-      value="livebps"
-    >
-      LiveBonus
-    </ToggleButton>
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Switch
+            color="primary"
+            disabled={showLiveBonusDisabled}
+            defaultChecked={!showLiveBonusDisabled}
+            onChange={() =>
+              dispatch({
+                type: "SET_SHOW_LIVE_BONUS",
+                payload: !showLiveBonus,
+              })
+            }
+          />
+        }
+        label="LiveBonus"
+        labelPlacement="bottom"
+      />
+    </FormGroup>
+    // <ToggleButton
+    //   selected={showLiveBonus && !showLiveBonusDisabled}
+    //   onChange={() =>
+    //     dispatch({
+    //       type: "SET_SHOW_LIVE_BONUS",
+    //       payload: !showLiveBonus,
+    //     })
+    //   }
+    //   disabled={showLiveBonusDisabled}
+    //   value="livebps"
+    // >
+    //   LiveBonus
+    // </ToggleButton>
   );
 };
 
