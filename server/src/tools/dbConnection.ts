9e1c12161ed58752367baa891c704850ef3f1e17
Bergo <bergo@tutanota.com>
2022-10-10 22:27:10 +0300

    mongouri

--- server/src/tools/dbConnection.ts
+++ server/src/tools/dbConnection.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI || "";
 
-const dbName = "fplbasket";
-
+// const dbName = "fplbasket";
+console.log("MONGO_URI:", MONGO_URI);
 async function dbConnect() {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-  return mongoose.connect(MONGO_URI + dbName);
+  return mongoose.connect(MONGO_URI);
 }
 
 export default dbConnect;
