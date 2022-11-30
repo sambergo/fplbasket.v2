3f5a4feb9de8b830c0b9894eef4524475d107d78
Bergo <bergo@tutanota.com>
2022-11-30 15:25:40 +0200

    build

--- build-and-git-commit-all.sh
+++ build-and-git-commit-all.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-rm -rf ./server/dist/
-cd ./server/
-tsc
-echo "tsc done"
-cp .env ./dist/
-cd ..
-pwd
-cd ./web/
-yarn build
-cd ..
-pwd
-mv ./web/build ./server/
+sh ./just-build.sh
 git add .
 git commit -m "build"
 git push
