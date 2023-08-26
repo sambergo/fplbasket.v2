0bdf14a92a84c0a9426bad7d7fb98d05801220e9
Bergo <bergo@tutanota.com>
2023-08-26 23:36:44 +0300

    add trycatch

--- server/src/tools/fetchBssData.ts
+++ server/src/tools/fetchBssData.ts
@@ -2,15 +2,20 @@ import { DataType } from "src/types/bssData";
 import superagent from "superagent";
 
 export const fetchBssDataFromFpl = async (): Promise<any> => {
-  const bootstrap_static = await superagent.get(
-    `https://fantasy.premierleague.com/api/bootstrap-static/`
-  );
-  const fpldata: DataType = bootstrap_static.body;
-  // Järjestää elementit id mukaan, joka on tarpeen frontissa
-  let elements: DataType["elements"] = [];
-  fpldata.elements.forEach((element) => {
-    elements[element.id] = element;
-  });
-  fpldata.elements = elements;
-  return fpldata;
+  try {
+    const bootstrapStatic = await superagent.get(
+      `https://fantasy.premierleague.com/api/bootstrap-static/`
+    );
+    const fplData: DataType = bootstrapStatic.body;
+    // Järjestää elementit id mukaan, joka on tarpeen frontissa
+    let elements: DataType["elements"] = [];
+    fplData.elements.forEach((element) => {
+      elements[element.id] = element;
+    });
+    fplData.elements = elements;
+    return fplData;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
 };
