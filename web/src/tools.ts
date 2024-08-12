316a9f7ac9385a5f183cc840784395ae07ae9530
Bergo <bergo@tutanota.com>
2024-08-12 18:45:30 +0300

    if season not started return one first gw

--- web/src/tools.ts
+++ web/src/tools.ts
@@ -3,7 +3,7 @@ import { ElementLive, LiveData } from "./types/livedata";
 import { PlayerPick } from "./types/newleague";
 
 export const getPlayerName = (
-  element: DataType["elements"][0] | null
+  element: DataType["elements"][0] | null,
 ): string => {
   if (!element) return "";
   const first_name =
@@ -16,7 +16,7 @@ export const getPlayerName = (
 };
 
 export const getPlayerWebName = (
-  element: DataType["elements"][0] | null
+  element: DataType["elements"][0] | null,
 ): string => {
   if (!element) return "";
   return element.web_name;
@@ -44,7 +44,7 @@ export const getElementType = (a: DataType["elements"][0] | undefined) => {
 
 export const getElementsTeam = (
   element: DataType["elements"][0],
-  teams: DataType["teams"]
+  teams: DataType["teams"],
 ) => {
   const team = teams[element.team - 1].short_name || "no";
   return team;
@@ -52,6 +52,9 @@ export const getElementsTeam = (
 
 export const getGWs = (events: DataType["events"]): DataType["events"] => {
   let gws = events.filter((e) => e.finished);
+  if (gws.length == 0) {
+    return [events[0]];
+  }
   const delay = 20 * 60 * 1000;
   for (let e of events) {
     // @ts-ignore
@@ -76,14 +79,14 @@ export const stillToPlay = (pick: number, liveData: LiveData): string => {
 
 export const fromTeamToPlay = (
   liveData: LiveData,
-  picks: PlayerPick[]
+  picks: PlayerPick[],
 ): string => {
   const picksStillToPlay = picks.reduce((prev, curr) => {
     const element = liveData.elements[curr.element];
     if (!element || curr.multiplier < 1) return prev;
     const fixture =
       element.explain.filter(
-        (e) => !liveData.fixtures[e.fixture]?.finished_provisional
+        (e) => !liveData.fixtures[e.fixture]?.finished_provisional,
       ).length + prev;
     return fixture;
   }, 0);
@@ -98,7 +101,7 @@ export const fromTeamToPlay = (
 
 export const getElementPoints = (
   element: ElementLive | null,
-  showLiveBonus: boolean
+  showLiveBonus: boolean,
 ): number => {
   if (!element) return 0;
   const minusLiveBonus = showLiveBonus ? 0 : element.live_bps ?? 0;
@@ -108,7 +111,7 @@ export const getElementPoints = (
 export const getArrow = (
   old_rank: number,
   i: number,
-  managersLength: number
+  managersLength: number,
 ): number => {
   if (old_rank < i) return 0;
   else if (old_rank === i) return 1;
@@ -120,7 +123,7 @@ export const getSomeFixtureIsActive = (liveData: LiveData | null): boolean => {
   console.log("getactive");
   if (!liveData?.fixtures) return true;
   const vastaus = !liveData.fixtures.some(
-    (fix) => fix?.started && !fix.finished
+    (fix) => fix?.started && !fix.finished,
   );
   console.log("vastaus:", vastaus);
   return vastaus;
