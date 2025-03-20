2b003a95f70da719cf66516306acbf99ffa1f562
Bergo <bergo@tutanota.com>
2025-03-20 11:25:36 +0200

    update reactdom

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,19 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { StateProvider, reducer } from "./state";
 import App from "./App";
 import theme from "./theme";
 
-ReactDOM.render(
+const container = document.querySelector("#root");
+if (!container) throw new Error("Failed to find the root element");
+const root = createRoot(container);
+
+root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <StateProvider reducer={reducer}>
       <App />
     </StateProvider>
-  </ThemeProvider>,
-  document.querySelector("#root")
+  </ThemeProvider>
 );
